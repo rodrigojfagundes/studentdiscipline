fdce51d58c983231143f172608e6a55f5ffd4d10
Rodrigo Jose Fagundes <rodrigojosefagundes@gmail.com>
2024-01-04 23:24:15 -0300

    Resource Discipline created

--- backend/studentdiscipline/src/main/java/io/github/rodrigojfagundes/studentdiscipline/resources/DisciplineResource.java
+++ backend/studentdiscipline/src/main/java/io/github/rodrigojfagundes/studentdiscipline/resources/DisciplineResource.java
@@ -1,5 +1,70 @@
 package io.github.rodrigojfagundes.studentdiscipline.resources;
 
-public class DisciplineResource {
+import java.net.URI;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import io.github.rodrigojfagundes.studentdiscipline.dto.DisciplineDTO;
+import io.github.rodrigojfagundes.studentdiscipline.services.DisciplineService;
+
+@RestController
+@RequestMapping(value = "/disciplines")
+public class DisciplineResource {
+	
+	@Autowired
+	private DisciplineService service;
+	
+	@GetMapping
+	public ResponseEntity<Page<DisciplineDTO>> findAll(
+			@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "12")
+			Integer linesPerPage,
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction,
+			@RequestParam(value = "orderBy", defaultValue = "name") String orderBy
+			) {
+		
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		
+			//TA ESCRITO LIST MAS NA VDD e UM PAGE
+		Page<DisciplineDTO> list = service.findAllPaged(pageRequest);
+		
+		return ResponseEntity.ok().body(list);
+		
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<DisciplineDTO> findById(@PathVariable Long id) {
+		DisciplineDTO dto = service.findById(id);
+		
+		return ResponseEntity.ok().body(dto);
+	}
+	
+	@PostMapping
+	public ResponseEntity<DisciplineDTO> insert(@RequestBody DisciplineDTO dto){
+		dto = service.insert(dto);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+		
+		return ResponseEntity.created(uri).body(dto);
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<DisciplineDTO> update(@PathVariable Long id, @RequestBody DisciplineDTO dto)	{
+		dto = service.update(id, dto);
+		
+		return ResponseEntity.ok().body(dto);
+	}
 }
